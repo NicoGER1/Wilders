a3985ac902a6c53caff7cbd0fdb31d7a91c1ac3c
NicoGER1 <nico3g@hotmail.fr>
2024-03-19 11:01:44 +0100

    update padmey to padme

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 import chalk from "chalk";
 
-console.log (
-    chalk.blue("Max ? Merite la carbonite")
-);
-console.log (
-    chalk.red("Nico J passe du coté obscur")
-);
-console.log (
-    chalk.yellow("Antho plus fan de C3PO que de Padmey")
-);
-console.log (
-    chalk.green("Pierre maitrise la force")
-);
+console.log(chalk.blue("Max ? Merite la carbonite"));
+console.log(chalk.red("Nico J passe du coté obscur"));
+console.log(chalk.yellow("Antho plus fan de C3PO que de Padme"));
+console.log(chalk.green("Pierre maitrise la force"));
