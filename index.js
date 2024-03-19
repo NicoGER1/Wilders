6e21ac4791efdbafdc1c5d266bf988d94d74adfb
NicoGER1 <nico3g@hotmail.fr>
2024-03-19 10:58:30 +0100

    exercice done

---
+++ index.js
@@ -0,0 +1,14 @@
+import chalk from "chalk";
+
+console.log (
+    chalk.blue("Max ? Merite la carbonite")
+);
+console.log (
+    chalk.red("Nico J passe du coté obscur")
+);
+console.log (
+    chalk.yellow("Antho plus fan de C3PO que de Padmey")
+);
+console.log (
+    chalk.green("Pierre maitrise la force")
+);
